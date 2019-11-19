1bb76094e6edf3ae3e88af3a0991958bde8feb15
liubats <dreamvendor1994@gmail.com>
2019-11-19 15:46:55 +0300

    Change

--- src/reducer.js
+++ src/reducer.js
@@ -1,48 +1,59 @@
+const reducer = (state = {
+    items: [{ label: 'Drink', done: false, id: '1' }, { label: 'Read', done: false, id: '2' }],
+    searchText: '',
+    newItemLabel: '',
+}, action) => {
+    console.log(action);
 
-const reducer = (state = {items:[{label:'Drink',done:true, id:'1'},{label:'Read',done:false,id:'2'}], 
-                          searchText:'',
-                          newItemLabel:'',
-                        }, action) => {
-                            console.log(action);
-                            
     switch (action.type) {
-        case 'CHANGE_SEARCH_TEXT': {
-            return {
-                ...state, searchText: action.text, items: [...state.items]
-            };
-        }
-        case 'CHANGE_NEW_ITEM_LABEL': {
-            return {
-                ...state, newItemLabel: action.label, items: [...state.items]
-            };
-        }
-        case 'ITEM_DONE_TOGGLE': {
-            const searchItemIndex = state.items.findIndex((item) => {
-                return item.id === action.id;
-            });
-            const searchItem = state.items[searchItemIndex];
-            const newItem = { ...searchItem, done: action.done };
-            let leftPart=state.items.slice(0, searchItemIndex);
-            let rightPart=state.items.slice(searchItemIndex+1);
-            const newItems = [...leftPart, {...newItem}, ...rightPart];
+        case 'CHANGE_SEARCH_TEXT':
+            {
+                return {
+                    ...state,
+                    searchText: action.text,
+                    items: [...state.items]
+                };
+            }
+        case 'CHANGE_NEW_ITEM_LABEL':
+            {
+                return {
+                    ...state,
+                    newItemLabel: action.label,
+                    items: [...state.items]
+                };
+            }
+        case 'ITEM_DONE_TOGGLE':
+            {
+                const searchItemIndex = state.items.findIndex((item) => {
+                    return item.id === action.id;
+                });
+                const searchItem = state.items[searchItemIndex];
+                const newItem = {...searchItem, done: action.done };
+                let leftPart = state.items.slice(0, searchItemIndex);
+                let rightPart = state.items.slice(searchItemIndex + 1);
+                const newItems = [...leftPart, {...newItem }, ...rightPart];
 
-            return { 
-                ...state,
-                items:newItems
-            };
-                
-        }
-        case 'ADD_NEW_ITEM': {
-            return {
-            ...state, 
-            items: [...state.items, {label: action.label,
-                                    id: action.id}],
-            newItemLabel: '',
-            };
-        }
-        default: {
-            return state;
-        }
+                return {
+                    ...state,
+                    items: newItems
+                };
+
+            }
+        case 'ADD_NEW_ITEM':
+            {
+                return {
+                    ...state,
+                    items: [...state.items, {
+                        label: action.label,
+                        id: action.id
+                    }],
+                    newItemLabel: '',
+                };
+            }
+        default:
+            {
+                return state;
+            }
     }
 }
 
