218264c2a3d6d11f535e83b0bf19d30cf0655972
liubats <dreamvendor1994@gmail.com>
2019-11-20 16:17:01 +0300

    made list

---
+++ src/components/moleculas/SearchPanel/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Input } from 'antd';
+import { connect } from 'react-redux';
+
+
+class SearchPanel extends Component {
+    render() {
+        console.log(this.props);
+        const obj = {};
+        obj.type = 'CHANGE_SEARCH_TEXT';
+
+        return ( <
+            Input placeholder = "Find event"
+            onChange = {
+                (e) => {
+                    obj.text = e.target.value;
+                    this.props.dispatch(obj);
+                }
+            }
+
+            value = { this.props.searchText }
+            />
+        )
+    }
+}
+
+const mapStateToProps = function(state) {
+    const props = {
+        searchText: state.searchText,
+    };
+
+    return props;
+}
+
+export default connect(mapStateToProps)(SearchPanel);
